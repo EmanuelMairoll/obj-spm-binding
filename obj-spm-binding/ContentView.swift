53bd41c1edc2e3ef7790b00b2ed138a0d328453d
Emanuel Mairoll <emanuel.mairoll@authenticvision.com>
2024-03-06 12:43:40 +0100

    Initial Commit

---
+++ obj-spm-binding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  obj-spm-binding
+//
+//  Created by Emanuel Mairoll on 06.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ obj-spm-binding/obj_spm_bindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  obj_spm_bindingApp.swift
+//  obj-spm-binding
+//
+//  Created by Emanuel Mairoll on 06.03.24.
+//
+
+import SwiftUI
+
+@main
+struct obj_spm_bindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
